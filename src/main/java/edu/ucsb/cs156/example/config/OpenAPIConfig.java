d9fad53e88cf5f445fd24bc8eb939ef2312f7d2a
Dagin Gregory <dagin.gregory@gmail.com>
2023-11-02 14:10:43 -0700

    Fixed conflict

--- src/main/java/edu/ucsb/cs156/example/config/OpenAPIConfig.java
+++ src/main/java/edu/ucsb/cs156/example/config/OpenAPIConfig.java
@@ -6,11 +6,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
   info = @Info(
-<<<<<<< HEAD
   title = "UCSB CMPSC 156 f23, team03",
-=======
-  title = "UCSB CMPSC 156 f23, team02",
->>>>>>> c71d0c63bc4382959325d08774b23745c6e4c177
   description = """
     <p><a href='/'>Home Page</a></p>
     <p><a href='/h2-console'>H2 Console (only on localhost)</a></p>
