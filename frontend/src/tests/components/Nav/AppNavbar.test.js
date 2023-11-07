fa022ef36fcc835096a011a3e67eaa9efbc0a264
Alexander Chau <apchau@ucsb.edu>
2023-11-07 14:57:46 -0800

    Added testing for the Navbar part of Recommendation Requests and changed name to be plural

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -170,7 +170,28 @@ describe("AppNavbar tests", () => {
         expect(link.getAttribute("href")).toBe("/restaurants");
     });
 
-    test("Restaurant and UCSBDates links do NOT show when not logged in", async () => {
+    test("renders the recommendationrequest link correctly", async () => {
+
+        const currentUser = currentUserFixtures.userOnly;
+        const systemInfo = systemInfoFixtures.showingBoth;
+
+        const doLogin = jest.fn();
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await screen.findByText("Recommendation Requests");
+        const link = screen.getByText("Recommendation Requests");
+        expect(link).toBeInTheDocument();
+        expect(link.getAttribute("href")).toBe("/recommendationrequest");
+    });
+
+    test("Restaurant and UCSBDates and RecommendationRequest links do NOT show when not logged in", async () => {
         const currentUser = null;
         const systemInfo = systemInfoFixtures.showingBoth;
         const doLogin = jest.fn();
@@ -184,6 +205,7 @@ describe("AppNavbar tests", () => {
         );
 
         expect(screen.queryByText("Restaurants")).not.toBeInTheDocument();
+        expect(screen.queryByText("Recommendation Requests")).not.toBeInTheDocument();
         expect(screen.queryByText("UCSBDates")).not.toBeInTheDocument();
     });
 
