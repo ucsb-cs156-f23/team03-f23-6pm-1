27587a8a8b33fc30f399f140d7c5bf03a877bffa
Erwan Caron <erwan@169-231-114-76.wireless.ucsb.edu>
2023-11-08 11:53:44 -0800

    displaying booleans in table

--- frontend/src/stories/components/HelpRequests/HelpRequestsTable.stories.js
+++ frontend/src/stories/components/HelpRequests/HelpRequestsTable.stories.js
@@ -18,7 +18,7 @@ const Template = (args) => {
 export const Empty = Template.bind({});
 
 Empty.args = {
-    restaurants: []
+    requests: []
 };
 
 export const ThreeItemsOrdinaryUser = Template.bind({});
