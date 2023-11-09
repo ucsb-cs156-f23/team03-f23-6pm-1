9787d62f392c96ca634920f8460bc4469e762a27
jakedel <42163241+jakedel@users.noreply.github.com>
2023-11-08 19:19:20 -0800

    add dc menu item table component, stories, tests

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,33 +1,33 @@
 const ucsbDiningCommonsMenuItemFixtures = {
   oneDiningCommonsMenuItem: [
     {
-      "id": 1,
-      "diningCommonsCode": "1",
-      "name": "testItem1",
-      "station": "station1"
-    }
+      id: 1,
+      diningCommonsCode: "1",
+      name: "testItem1",
+      station: "station1",
+    },
   ],
 
   threeDiningCommonsMenuItems: [
     {
-      "id": 2,
-      "diningCommonsCode": "1",
-      "name": "testItem2",
-      "station": "station1"
+      id: 1,
+      diningCommonsCode: "1",
+      name: "testItem2",
+      station: "station1",
     },
     {
-      "id": 3,
-      "diningCommonsCode": "1",
-      "name": "testItem3",
-      "station": "station1"
+      id: 2,
+      diningCommonsCode: "1",
+      name: "testItem3",
+      station: "station1",
     },
     {
-      "id": 4,
-      "diningCommonsCode": "1",
-      "name": "testItem4",
-      "station": "station2"
-    }
+      id: 3,
+      diningCommonsCode: "1",
+      name: "testItem4",
+      station: "station2",
+    },
   ],
-}
+};
 
 export { ucsbDiningCommonsMenuItemFixtures };
